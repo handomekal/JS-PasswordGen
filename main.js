f2389d1be81dad73e8410ec79579f43eb0fa3175
handomekal <handomekal@gmail.com>
2020-03-17 13:23:40 -0400

    Created event listner and generator function

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+// DOM Elements
+const resultEL = document.getElementById('result')
+const lengthEL = document.getElementById('length')
+const upperCaseEL = document.getElementById('uppercase')
+const lowerCaseEL = document.getElementById('lowercase')
+const numbersEL = document.getElementById('numbers')
+const symbolsEL = document.getElementById('symbols')
+const generateEL = document.getElementById('generate')
+const clipboardEL = document.getElementById('clipboard')
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+generateEL.addEventListener('click', () => {
+    const length = +lengthEL.value;
+    const hasLower = lowerCaseEL.checked;
+    const hasUpper = upperCaseEL.checked;
+    const hasNumber = numbersEL.checked;
+    const hasSymbol = symbolsEL.checked;
+
+    console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+})
+
 //  Generator Functions
 
 function getRandomLower() {
@@ -21,4 +48,4 @@ function getRandomSymbol() {
     
 }
 
- console.log(getRandomSymbol());
+//  console.log(getRandomSymbol());
